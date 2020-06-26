fdf964267b9acf6dc4ba06c18d2f88d6aa8fb1ff
IsaiahPatton <isaiahpatton1@gmail.com>
2020-06-25 22:17:46 -0400

    Fix errors

--- src/main/java/stdlibport/client/IsaiahTv.java
+++ src/main/java/stdlibport/client/IsaiahTv.java
@@ -1,15 +1,10 @@
 package stdlibport.client;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 public class IsaiahTv extends JFrame {
 
     public IsaiahTv() {
-        JPanel p = new JPanel();
-        p.add(new JLabel("test JLabel"));
-        setContentPane(p);
     }
 
 }
